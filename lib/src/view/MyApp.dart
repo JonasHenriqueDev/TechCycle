002d03970a502ca77511b936d19735b859baee79
Pedro H. Mota <pedro.rito@upe.br>
2024-06-26 23:05:38 -0300

    estruturacao das pastas

---
+++ lib/src/view/MyApp.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
