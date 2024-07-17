4bd61b113e93ee11638be93a13962761f62e4281
Pedro H. Mota <pedro.rito@upe.br>
2024-07-17 15:41:21 -0300

    feat: página de perfil do usuário

--- lib/src/view/ProfilePage.dart
+++ lib/src/view/ProfilePage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
+
+  void callbackFuntion() {}
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +13,162 @@ class ProfilePage extends StatelessWidget {
         backgroundColor: Colors.green,
         iconTheme: IconThemeData(color: Colors.white),
       ),
-      body: const Center(
-        child: Text('Bem-vindo à sua conta!'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 60,
+              backgroundImage: AssetImage(
+                  'assets/images/fotoperfil.png'), // Substitua pela URL da sua imagem de perfil
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Jonas Henrique',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Garanhuns, Pernambuco',
+              style: TextStyle(fontSize: 18, color: Colors.grey[700]),
+            ),
+            Divider(color: Colors.grey[400], thickness: 1, height: 40),
+            Container(
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '999 descartes corretos realizados',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    '100 recompensas resgatadas',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ElevatedButton(
+                    onPressed: null,
+                    style: ElevatedButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    child: const Row(
+                      children: [
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'jonas.henrique@gmail.com',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              SizedBox(width: 10), // Espaço entre os textos
+                              Text(
+                                'Alterar e-mail',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Icon(Icons.arrow_forward),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: null,
+                    style: ElevatedButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    child: const Row(
+                      children: [
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                '(87) 9 9999-9999',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              SizedBox(width: 10), // Espaço entre os textos
+                              Text(
+                                'Alterar telefone',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Icon(Icons.arrow_forward),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: null,
+                    style: ElevatedButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Nome, CPF,...',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              SizedBox(width: 10), // Espaço entre os textos
+                              Text(
+                                'Alterar dados pessoais',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Icon(Icons.arrow_forward),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 15),
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextButton(
+                      onPressed: null,
+                      child: Text(
+                        'Sair da conta',
+                        style: TextStyle(fontSize: 18, color: Colors.red),
+                      ))
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
